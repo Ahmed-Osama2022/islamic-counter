de3656b83fafd2306c8e895e460b85a8075ff1c3
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-07-06 20:33:52 +0300

    Restructure the files

---
+++ Old/main.js
@@ -0,0 +1,59 @@
+// This script was made by 'Ahmed Osama' in 2023 May
+
+/**
+ * Global variables
+ */
+const counter = document.querySelector('.counter');
+const button = document.querySelector('.increment');
+const reset = document.querySelector('#reset');
+const word = document.querySelector('.word');
+
+// Local Storage
+let countLocal = localStorage.getItem('count') || 0;
+
+/**
+ * End global variables
+ */
+
+
+/**
+ * Start helper code 
+ */
+
+counter.textContent = countLocal;
+// Start an Event listener on the buttons
+
+// 1- Increment button: 
+button.addEventListener('click', (e) => {
+    countLocal ++;
+    localStorage.setItem('count', countLocal);
+    counter.textContent = countLocal;
+    
+    // checking conditions:
+    if (countLocal === 1000) {
+        word.textContent = 'لقد انتهيت من العد';
+
+        setTimeout(() => {
+            word.textContent = 'الصلاة على النبي محمد' }
+            , 1500); 
+        if (countLocal === 1000) {
+            countLocal = 0;
+        };
+    };
+});
+// 2- Reset button: 
+reset.addEventListener('click', (e) => {
+    countLocal = 0;
+    counter.textContent = 0;
+    localStorage.removeItem('count');
+});
+
+
+/**
+ * End start helper code 
+ */
+
+
+/**
+ * End of the script
+ */
